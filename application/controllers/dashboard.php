429127de1fc04d0144211ca6a24be3f89fb91397
jefri febrianto <mborgud46@gmail.com>
2020-07-13 14:51:08 +0700

    Initial commit

---
+++ application/controllers/dashboard.php
@@ -0,0 +1,37 @@
+<?php
+class Dashboard extends CI_Controller{
+    public function index()
+    {
+        $data['barang'] = $this->model_barang->tampil_data()->result();
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
+        $this->load->view('dashboard', $data);
+        $this->load->view('templates/footer');  
+    }
+
+    public function tambah_ke_keranjang($id)
+    {
+        $barang = $this->model_barang->find($id);
+
+        $data = [
+            'id'     =>  $barang->id_brg,
+            'qty'    =>   1,
+            'price'  =>  $barang->harga,
+            'name'   =>  $barang->nama_brg,
+            
+        ];
+
+        $this->cart->insert($data);
+        redirect('dashboard');
+    }
+
+    public function detail_keranjang()
+    {
+    $this->load->view('templates/header');
+    $this->load->view('templates/sidebar');
+    $this->load->view('keranjang');
+    $this->load->view('templates/footer');  
+    }
+
+}
+?>
